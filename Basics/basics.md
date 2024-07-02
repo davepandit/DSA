557d5ee20c2e751adffecdf78f781c5475968f9a
Debajyoti Pandit <codeninja0812@gmail.com>
2024-07-02 20:52:11 +0530

    Basic patterns done

---
+++ Basics/basics.md
@@ -0,0 +1,9 @@
+## 🚀When the range is from 10^-9 to 10^9 then we can use int say and when the range is from 10^-12 to 10^12 then we can use long and if the range is from 10^-18 to 10^18 then we can use lon long 
+
+## 🚀Second step is learning about STL in C++ => Baiscally STL in C++ can be considered like the packages that we have used for the last many months, so basically their task is to reduce human effort and be more productive by providing templates mane ek kaam ko scratch se kyu hi karne ka when there are many stuffs available to help us that's the point
+
+## 🚀So basically STL comprises of 4 things =>
+🚀containers
+🚀iterators
+🚀algorithms(matlab aaj agar mere ko ek sorting ka function likhna hai then I will not write it from scratch rahter will be writing that using the template provided by the STL)
+🚀functions or functors
--- Basics/q_01.cpp
+++ Basics/q_01.cpp
@@ -1,7 +1,207 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+// p_01
+void pattern_01(){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            cout<<"# ";
+        }
+        cout<<endl;
+    }
+}
 
+void pattern_02(){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<=i; j++){
+            cout<<"# ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_03(){
+    for(int i=1; i<=3; i++){
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_04(){
+    for(int i=1; i<=3; i++){
+        for(int j=1; j<=i; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_05(){
+    for(int i=3; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            cout<<"# ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_06(){
+    for(int i=3; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_07(int n){
+    for(int i=1; i<=n; i++){
+        //spaces 
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        //stars
+        for(int k=1; k<=(2*i)-1; k++){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_08(int n){
+    for(int i=n; i>=1; i--){
+        //spaces 
+        for(int j=1; j<n-i+1; j++){
+            cout<<" ";
+        }
+        //stars
+        for(int k=(2*i)-1; k>=1; k--){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_09(int n){
+    for(int i=1; i<=n; i++){
+        //spaces 
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        //stars
+        for(int k=1; k<=(2*i)-1; k++){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+    for(int l=n; l>=1; l--){
+        //spaces 
+        for(int j=1; j<n-l+1; j++){
+            cout<<" ";
+        }
+        //stars
+        for(int k=(2*l)-1; k>=1; k--){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_10(){
+    for(int i=0; i<2; i++){
+        for(int j=0; j<=i; j++){
+            cout<<"# ";
+        }
+        cout<<endl;
+    }
+    for(int l=3; l>=1; l--){
+        for(int j=1; j<=l; j++){
+            cout<<"# ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_11(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            if((i+j)%2 != 0){
+                cout<< 0;
+            }else{
+                cout<< 1;
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_12(int n){
+    for(int i=1; i<=n; i++){
+        int dummy = i;
+        //first triangle
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        //second triangle spaces
+        for(int spaces=0; spaces<(2*i)+2; spaces++){
+            cout<<' ';
+        }
+        //second triangle stars
+        for(int stars=1; stars<=i; stars++){
+            cout<<stars;
+        }
+        //now go to the new line
+        cout<<endl;
+        dummy-=1;
+    }
+}
+
+void pattern_13(int n){
+    for(int i=1; i<=n; i++){
+        int dummy = i;
+        // first triangle 
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+
+        //second triangle
+        for(int spaces=1; spaces<=n-i; spaces++){
+            //print a space
+            cout<<' ';
+        }
+
+        //third triangle
+        //spaces
+        for(int spaces=1; spaces<=n-i; spaces++){
+            //print the spaces
+            cout<<' ';
+        }
+        //again numbers
+        for(int num=1; num<=i; num++){
+            cout<<dummy;
+            dummy = dummy-1;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_14(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if((i==2 && j==2) || (i==2 && j==3) || (i==3 && j==2) || (i==3 && j==3)){
+                cout<<' ';
+            }else{
+                cout<< '*';
+            }
+        }
+        cout<<endl;
+    }
+}
+
+
+int main(){
+ pattern_14(4);
  return 0;
 }
